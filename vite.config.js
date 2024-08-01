a7f2dbdbd6deee5cf2f720d77f85409bc51c59f3
Luis Carvalho <39739462+LuisFonsecaCarvalho@users.noreply.github.com>
2024-08-01 18:02:25 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/Portfolio/",
-  build: {
-    rollupOptions: {
-      external: ['swiper'], // Add 'swiper' here
-      external: ['swiper/swiper-bundle.css'], // Add 'swiper/swiper-bundle.css' here
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: ['swiper'], // Add 'swiper' here
+  //     external: ['swiper/swiper-bundle.css'], // Add 'swiper/swiper-bundle.css' here
+  //   },
+  // },
 })
